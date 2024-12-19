5372bcc8dd6309ce8d3f861bd2cf4b3415038e69
Mesud-Ahmed <mesudahmed94@gmail.com>
2024-12-19 10:07:51 +0300

    feat: create a reusable Modal Component

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -43,12 +43,12 @@ export function CartContextProvider({ children }) {
 
     const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] })
 
-    
+
     function addItem(item) {
         dispatchCartAction({ type: 'ADD_ITEM', item })
     }
-    function removeItem(id){
-        dispatchCartAction({type:'REMOVE_ITEM',id})
+    function removeItem(id) {
+        dispatchCartAction({ type: 'REMOVE_ITEM', id })
     }
 
     const contextValue = {
