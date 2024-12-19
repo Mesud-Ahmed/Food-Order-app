f4ed98b4842ae7bd6377f71cf2cfa6e7d9aecd80
Mesud-Ahmed <mesudahmed94@gmail.com>
2024-12-19 16:12:05 +0300

    feat: add a separate component to display the cart items

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,14 +4,14 @@ import { useContext } from "react";
 import CartContext from "../store/CartContext";
 import Button from "./UI/Button";
 import UserProgressContext from "../store/UserProgressContext";
-
+import CartItem from "./CartItem";
 export default function Cart() {
     const cartCtx = useContext(CartContext)
     const userProgressCtx = useContext(UserProgressContext)
 
     const totPrice = cartCtx.items.reduce((total, item) => total + item.quantity * item.price, 0)
 
-    function handleCloseCart(){
+    function handleCloseCart() {
         userProgressCtx.hideCart()
     }
     return (
@@ -19,7 +19,13 @@ export default function Cart() {
             <h2>Your Cart</h2>
             <ul>
                 {cartCtx.items.map((item) => (
-                    <li key={item.id}>{item.name} - {item.quantity}</li>
+                    <CartItem
+                        key={item.id}
+                        name={item.name}
+                        quantity={item.quantity}
+                        price={item.price}
+                        
+                    />
                 ))}
             </ul>
             <p className="cart-total">{currencyFormatter.format(totPrice)}</p>
