f7f1fbc8a742c634851b5051f9d4a63a01dfceed
Mesud-Ahmed <mesudahmed94@gmail.com>
2024-12-20 11:34:48 +0300

    fix: center the loading meals text in Meals.jsx

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -4,7 +4,7 @@ const requestConfig = {}
 export default function Meals() {
     const { data: loadedMeals, isLoading, error } = useHttp('http://localhost:3000/meals',requestConfig, [])
     if (isLoading) {
-        return <p>Fetching meals...</p>
+        return <p className="center">Fetching meals...</p>
     }
 
     return (
