048abfa839f78c59478dbbfdac68891b20c86992
Mesud-Ahmed <mesudahmed94@gmail.com>
2024-12-21 03:54:04 +0300

    feat: add a separate component and styling to display any errors that might occur  while fetching the meals data

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,12 +1,15 @@
 import useHttp from "./hooks/useHttp"
 import MealItem from "./MealItem"
+import ErrorPage from "./ErrorPage"
 const requestConfig = {}
 export default function Meals() {
-    const { data: loadedMeals, isLoading, error } = useHttp('http://localhost:3000/meals',requestConfig, [])
+    const { data: loadedMeals, isLoading, error } = useHttp('http://localhost:3000/mealsrrfsdfs', requestConfig, [])
     if (isLoading) {
         return <p className="center">Fetching meals...</p>
     }
-
+    if (error) {
+        return <ErrorPage title='Failed to fetch meals' message={error} />
+    }
     return (
         <ul id="meals">
             {loadedMeals.map((meal) => (
